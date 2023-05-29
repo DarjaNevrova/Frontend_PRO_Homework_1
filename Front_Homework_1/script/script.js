ee5b3ff72f190b8f98ca0f45ba585cc606f022d0
DarjaNevrova <122455394+DarjaNevrova@users.noreply.github.com>
2023-05-29 23:19:29 +0300

    Add files via upload

---
+++ Front_Homework_1/script/script.js
@@ -0,0 +1,40 @@
+// FrontEnd_Homework_one
+
+
+// 18 task:
+
+// function handler(arr) {
+// 	return arr.map(elem => elem < 0 ? -elem : elem)
+// }
+
+
+// 19 task:
+
+// function handler(arr) {
+//   return arr.filter((value, index) => value !== index);
+// }
+
+
+// 20 task:
+
+// function handler (arr) {
+// 	return arr.filter((elem, index, acc) => {
+//       return acc.indexOf(elem) === index
+//     })
+// }
+
+
+// 21 task:
+
+// function handler(arr) {
+//   return arr.map(str => str.length);
+// }
+
+
+
+// 22 task:
+
+// function handler(arr, number) {
+// 	const numbers = arr.filter(elem => typeof elem === 'number')
+//     return numbers.map(elem => elem ** number)
+// }
